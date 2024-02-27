0314c321664dcb26b259087ffe040ba9fa676601
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-02-27 12:35:23 +0500

    Add files via upload

---
+++ Alert assignment/app.js
@@ -0,0 +1,10 @@
+// alert("Error! Please Enter a valid Password")
+
+// alert("Welcome to JS Land...\n Happy Coding!");
+
+// alert("Welcome to JS Land...")
+
+// alert("Happy Coding!")
+
+// console.log("Hello World!")
+
