41a92d15c455656d567153ed04088c69b6a3f4d8
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-02-28 09:12:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// VARIABLES FOR STRINGS
+// =========================
+
+// var username;
+// var username="myname"
+// var myname = "Syed Asjad"
+// console.log(myname)
+// var message;
+// var message = "Hello World";
+// alert(message)
+// var studentBio;
+// var studentName = "Student Name: Syed Asjad"
+// var studnetAge = "Studnet Age: 26 years old"
+// var compltedCourse = "Complete Course: Web and app development"
+// alert(studentName);
+// alert(studnetAge);
+// alert(compltedCourse);
+// var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizza)
+// var email;
+// var email ="My email address is: Syedasjad12@gmail.com"
+// alert(email)
+// var book;
+// var book = "I am trying to learn from a book: A smarter way to learn javascript"
+// alert(book)
+// document.write("Yeah! I can write HTML content through javascript")
+// var emojis;
+// emojis = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(emojis)
+
+// VARIABLES FOR NUMBERS
+// ===========================
+
+// var age;
+// age = "I am 26 years old"
+// alert(age)
+// var visitWebpage = "You have visited this webpage 15 times"
+// alert(visitWebpage)
+// var birthYear = "My birth year is 1998</br> Data type of my declared variable is number"
+// document.write(birthYear)
+// var visitorName = "Syed Asjad"
+// var productTitle = "T-shirts"
+// var quantity = "5"
+// document.write(visitorName, "ordered", quantity, productTitle, "from XYZ store")
+
