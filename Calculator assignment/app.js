1ac34b197fc7f513c67f922936ca939f701e7be6
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-01 23:25:24 +0500

    Add files via upload

---
+++ Calculator assignment/app.js
@@ -0,0 +1,25 @@
+// Calculator Assignment
+
+
+let number1 = +prompt("Enter First Number:");
+let number2 = +prompt("Enter Second Number");
+let addition = number1 + number2;
+console.log("Sum of given numbers is ", addition, "\n");
+document.write("Sum of given numbers is: ", addition, "</br>");
+
+
+let subtraction = number1 - number2;
+console.log("Subtraction of given numbers is ", subtraction, "\n");
+document.write("Subtraction of given numbers is ", subtraction, "</br>");
+
+let multiplication = number1 * number2;
+console.log("multiplication of given numbers is ", multiplication, "\n");
+document.write("multiplication of given numbers is ", multiplication, "</br>");
+
+let division = number1 / number2;
+console.log("Division of given numbers is ", division, "\n");
+document.write("Division of given numbers is ", division, "</br>");
+
+let reminder = number1 % number2;
+console.log("Reminder of given numbers is ", reminder, "\n");
+document.write("Reminder of given numbers is ", reminder, "</br>");
