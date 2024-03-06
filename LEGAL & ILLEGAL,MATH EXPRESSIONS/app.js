87fcc2c630efa46209607bd617828e7beb0335e2
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-07 00:52:23 +0500

    Add files via upload

---
+++ LEGAL & ILLEGAL,MATH EXPRESSIONS/app.js
@@ -0,0 +1,53 @@
+// Declare 3 variables in one statement?
+
+// var num1,num2,num3;
+
+// Declare 5 legal & 5 illegal variable names.
+
+// legal 
+
+// var number_One;
+// number_One =4;
+// console.log(number_One)
+// var $myvar 
+// console.log($myvar)
+// var __myvar
+// console.log(__myvar)
+
+
+
+// illegal
+
+// var !num1
+// console.log(!num1)
+// var number one 
+// console.log(number one)
+// var 123num 
+// var var123;
+// console.log(123num)
+
+
+// Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// c) Variables must begin with a ______, ______ or
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// a) document.write("<h2>Rules for naming JS variables</h2>");
+
+// b) document.write("<p>Variable names can only contain: </p>","<p>upper case and lower case letters</p>", "<p>underscores sign</p>", "<p>dollar sign</p>", "<p>digits</p>",)
+
+// c) var $myvar,my_var 
+
+// d) var number_One;
+// number_one = 12;
+// case sensitive
+
+// e) var if;
+
+// console.log("if");
+
+// reserver keyword
+
