3512eb980cee31a1f7e0a2a5f5e24fccdc3d8dcf
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-10 01:44:54 +0500

    Update app.js

--- LEGAL & ILLEGAL,MATH EXPRESSIONS/app.js
+++ LEGAL & ILLEGAL,MATH EXPRESSIONS/app.js
@@ -1,51 +1,51 @@
-// Declare 3 variables in one statement?
+// Declare 3 letiables in one statement?
 
-// var num1,num2,num3;
+// let num1,num2,num3;
 
-// Declare 5 legal & 5 illegal variable names.
+// Declare 5 legal & 5 illegal letiable names.
 
 // legal 
 
-// var number_One;
+// let number_One;
 // number_One =4;
 // console.log(number_One)
-// var $myvar 
-// console.log($myvar)
-// var __myvar
-// console.log(__myvar)
+// let $mylet 
+// console.log($mylet)
+// let __mylet
+// console.log(__mylet)
 
 
 
 // illegal
 
-// var !num1
+// let !num1
 // console.log(!num1)
-// var number one 
+// let number one 
 // console.log(number one)
-// var 123num 
-// var var123;
+// let 123num 
+// let let123;
 // console.log(123num)
 
 
 // Display this in your browser
-// a) A heading stating “Rules for naming JS variables”
-// b) Variable names can only contain ______, ______,
+// a) A heading stating “Rules for naming JS letiables”
+// b) letiable names can only contain ______, ______,
 // ______ and ______.
-// c) Variables must begin with a ______, ______ or
-// d) Variable names are case _________
-// e) Variable names should not be JS _________
+// c) letiables must begin with a ______, ______ or
+// d) letiable names are case _________
+// e) letiable names should not be JS _________
 
-// a) document.write("<h2>Rules for naming JS variables</h2>");
+// a) document.write("<h2>Rules for naming JS letiables</h2>");
 
-// b) document.write("<p>Variable names can only contain: </p>","<p>upper case and lower case letters</p>", "<p>underscores sign</p>", "<p>dollar sign</p>", "<p>digits</p>",)
+// b) document.write("<p>letiable names can only contain: </p>","<p>upper case and lower case letters</p>", "<p>underscores sign</p>", "<p>dollar sign</p>", "<p>digits</p>",)
 
-// c) var $myvar,my_var 
+// c) let $mylet,my_let 
 
-// d) var number_One;
+// d) let number_One;
 // number_one = 12;
 // case sensitive
 
-// e) var if;
+// e) let if;
 
 // console.log("if");
 
@@ -56,38 +56,38 @@
 // MATH EXPRESSIONS
 
 
-// var number1 = +prompt("Enter first number:")
+// let number1 = +prompt("Enter first number:")
 
-// var numnber2 = +prompt("Enter second number:")
+// let numnber2 = +prompt("Enter second number:")
 
-// var addition = number1 + numnber2;
+// let addition = number1 + numnber2;
 
 // document.write(addition);
 // console.log(addition);
 
 
-// var lumber1 = +prompt("Enter First number");
+// let lumber1 = +prompt("Enter First number");
 
-// var lumber2 = +prompt("Enter second number");
+// let lumber2 = +prompt("Enter second number");
 
-// var result = lumber1 - lumber2;
+// let result = lumber1 - lumber2;
 
 // document.write(result);
 // console.log(result)
 
-// var a;
+// let a;
 
 // a = 10;
 
-// document.write("Value after variable declaration is: ", a, "</br>");
+// document.write("Value after letiable declaration is: ", a, "</br>");
 
 // document.write("Initial value: ", a , "</br>")
 
 // document.write("Value after increment is: ", ++a, "</br>");
 
-// var b = 2;
+// let b = 2;
 
-// var addition = (a + b);
+// let addition = (a + b);
 
 // document.write("Value after addition is :", addition, "</br>");
 
@@ -95,13 +95,13 @@
 
 // document.write("Value after division is: " , a/b, "</br>")
 
-// var price_of_movie_Ticket = 100;
-// var total_tickets_price = price_of_movie_Ticket * 5;
+// let price_of_movie_Ticket = 100;
+// let total_tickets_price = price_of_movie_Ticket * 5;
 // document.write("Total cost to buy 5 tickets to a movie is: ", total_tickets_price, "PKR")
 
 
 // document.write("<h2>Table of Five </h2>");
-// var Five = 5;
+// let Five = 5;
 // document.write("5"," x ", "1", " = ", Five*1, "</br>");
 // document.write("5"," x ", "2", " = ", Five*2,"</br>");
 // document.write("5"," x ", "3", " = ", Five*3,"</br>");
@@ -113,24 +113,65 @@
 // document.write("5"," x ", "9", " = ", Five*9,"</br>");
 // document.write("5"," x ", "10", " = ", Five*10,"</br>");
 
-// var cel = 20;
-// var fahrenheit = (cel*9/5) + 32;
+// let cel = 20;
+// let fahrenheit = (cel*9/5) + 32;
 // document.write(cel, "<sup>0</sup>" + "C " + fahrenheit + "<sup>0</sup>" + "F" + "</br>");
 
 // celsius = (fahrenheit - 32)*5/9;
 // document.write(fahrenheit, "<sup>0</sup>" + "F " + celsius  + "<sup>0</sup>" + "C" );
 
 
-// var price1 = +prompt("Enter price of item1");
-// var price2 = +prompt("Enter price of item2");
-// var quantity1 = +prompt("Ordered quantity of item 1");
-// var quantity2 = +prompt("Ordered quantity of item 2");
-// var shiping_Charges = 100;
-// var Total = (price1*quantity1) + (price2 * quantity2) + shiping_Charges;
+// let price1 = +prompt("Enter price of item1");
+// let price2 = +prompt("Enter price of item2");
+// let quantity1 = +prompt("Ordered quantity of item 1");
+// let quantity2 = +prompt("Ordered quantity of item 2");
+// let shiping_Charges = 100;
+// let Total = (price1*quantity1) + (price2 * quantity2) + shiping_Charges;
 // document.write("Price of item 1: " + price1 + "</br>");
 // document.write("Price of item 2: " + price2 + "</br>");
 // document.write("Quantity of item 1: " + quantity1 + "</br>");
 // document.write( "Quantity of item 2: " + quantity2 + "</br>");
 // document.write("Shipping Charges: " + shiping_Charges + "</br>");
 // document.write("Total cost of your order is: " + Total);
+// let total_Marks = +prompt("Enter Your Total Marks");
+// let obtained_Marks = +prompt("Enter Your Obtained Marks");
+// let percentage = obtained_Marks/total_Marks*100;
+// console.log("Your Percentage is: " + percentage);
+// document.write("Your Percentage is: " + percentage);
+// let us_dollar = 10;
+// us_dollar = 10*280;
+// let s_Riyal = 25;
+// s_Riyal = 25*1860;
+// total = us_dollar + s_Riyal;
+// document.write("Total Currency in PKR: " + total);
+// console.log("Total Currency in PKR: " + total);
+// let x = (5 + 5*10) /10;
+// document.write(x);
+// console.log(x);
+// let current_Year = 2024;
+// let birth_Year = 2000;
+// let age_result = current_Year - birth_Year;
+// document.write(age_result);
+// console.log("You are " + age_result + "Years Old");
+// let c_year = 2024;
+// let b_year = prompt("What is your birth year: ");
+// let c_age = c_year - b_year;
+// document.write ("Now You Are: " + c_age + " Years Old"); 
+// console.log ("Now You Are: " + c_age + " Years Old"); 
+// let cons = 3.142;
+// let radius = 20;
+// let circumference_of_a_circle = 2*cons*radius;
+// let area = cons*(radius*radius);
+// document.write("Radius of a Circle is: " + radius + "</br>");
+// document.write("The circumference is: " + circumference_of_a_circle + "</br>");
+// document.write("The area is: " + area )
+// let snaks = "cheezballs";
+// let current_age = 26;
+// let max_age = 65;
+// let left_years = max_age-current_age;
+// let days_in_year = 365;
+// let max_amount = 3;
+// let eat_Most_last = (days_in_year*max_amount*left_years);
+// document.write("You will need total: " + eat_Most_last + snaks  +   " of Snacks for rest of your life");
+// console.log("You will need total: " + eat_Most_last + snaks + " of Snacks for rest of your life ");
 
