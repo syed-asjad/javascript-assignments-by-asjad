bca4ec8d8d4350b5d77fa75210d4221e3207a399
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-18 20:37:50 +0500

    Add files via upload

---
+++ USER INPUT & CONDITIONAL/app.js
@@ -0,0 +1,16 @@
+// USER INPUT & CONDITIONAL STATEMENT
+
+let city_name = prompt("Enter a City Name").toLowerCase
+if(city_name == karachi){
+
+    console.log("Welcome to city of lights");
+
+    elseif(city_name == Lahore).toLowerCase
+
+    console.log("Welcome to city of Mughals");
+
+}
+else{
+    
+    console.log("You have Entered a wrong city");
+}
