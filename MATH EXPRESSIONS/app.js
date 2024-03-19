03e49296a2100df03fe676691984d8e1032266f9
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-19 17:40:05 +0500

    app.js

--- MATH EXPRESSIONS/app.js
+++ MATH EXPRESSIONS/app.js
@@ -31,7 +31,7 @@
 // console.log("Now the final Result is :", result);
 // let user_Name = prompt("Please Enter Your Name: ");
 // console.log(`Hi! ${user_Name} Welcome to Saylani`);
-// let value_for_table = +prompt("Enter Any Number and you will get the table of that Number");
+// let value_for_table = +prompt("Enter Any Number and you will get the table of that Numnber");
 // console.log(`${value_for_table} x 1 = ${value_for_table*1}`);
 // console.log(`${value_for_table} x 2 = ${value_for_table*2}`);
 // console.log(`${value_for_table} x 3 = ${value_for_table*3}`);
@@ -42,9 +42,27 @@
 // console.log(`${value_for_table} x 8 = ${value_for_table*8}`);
 // console.log(`${value_for_table} x 9 = ${value_for_table*9}`);
 // console.log(`${value_for_table} x 10 = ${value_for_table*10}`);
-// let subject_one = prompt("Enter the name of first subject");
-// let subject_two = prompt("Enter the name of second subject");
-// let subject_three = prompt("Enter the name of third subject");
-// let total_Marks = 100;
+// let subject_one = prompt("Enter the name of first subject").toLowerCase();
+// let subject_two = prompt("Enter the name of second subject").toLowerCase();
+// let subject_three = prompt("Enter the name of third subject").toLowerCase();
+// let total_Marks = 100*3;
+// let obt_marks_subject_one = +prompt(`Enter Obtained marks of: ${subject_one}`);
+// let obt_marks_subject_two = +prompt(`Enter Obtained marks of: ${subject_two}`);
+// let obt_marks_subject_three = +prompt(`Enter Obtained marks of: ${subject_three}`);
+// let obtained_Marks = obt_marks_subject_one + obt_marks_subject_two + obt_marks_subject_three;
+// let percentage = (obtained_Marks/total_Marks)*100;
 
+// // Displaying the Result
+// document.write("<h2>Result</h2>");
+// document.write("<table>");
 
+// document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+// document.write("<tr><td>" + subject_one + "</td><td>100</td><td>" + marks1 + "</td></tr>");
+// document.write("<tr><td>" + subject_two + "</td><td>100</td><td>" + marks2 + "</td></tr>");
+// document.write("<tr><td>" + subject_three + "</td><td>100</td><td>" + marks3 + "</td></tr>");
+// document.write("</table>");
+// document.write("<p>Total Marks: " + total_Marks + "</p>");
+// document.write("<p>Obtained Marks: " + obtained_Marks + "</p>");
+// document.write("<p>Percentage: " + percentage + "%</p>");
+
+// document.write("</table>");
