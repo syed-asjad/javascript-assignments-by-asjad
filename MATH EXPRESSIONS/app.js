6e86a69658e374094392ce796fab8cc56fc485aa
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-19 07:54:21 +0500

    app.js

--- MATH EXPRESSIONS/app.js
+++ MATH EXPRESSIONS/app.js
@@ -29,3 +29,22 @@
 // console.log("Third the Result of --a - --b + ++b + b--; is :", (--a) - (--b) + (++b) + (b--));
 // console.log("==========================================")
 // console.log("Now the final Result is :", result);
+// let user_Name = prompt("Please Enter Your Name: ");
+// console.log(`Hi! ${user_Name} Welcome to Saylani`);
+// let value_for_table = +prompt("Enter Any Number and you will get the table of that Number");
+// console.log(`${value_for_table} x 1 = ${value_for_table*1}`);
+// console.log(`${value_for_table} x 2 = ${value_for_table*2}`);
+// console.log(`${value_for_table} x 3 = ${value_for_table*3}`);
+// console.log(`${value_for_table} x 4 = ${value_for_table*4}`);
+// console.log(`${value_for_table} x 5 = ${value_for_table*5}`);
+// console.log(`${value_for_table} x 6 = ${value_for_table*6}`);
+// console.log(`${value_for_table} x 7 = ${value_for_table*7}`);
+// console.log(`${value_for_table} x 8 = ${value_for_table*8}`);
+// console.log(`${value_for_table} x 9 = ${value_for_table*9}`);
+// console.log(`${value_for_table} x 10 = ${value_for_table*10}`);
+// let subject_one = prompt("Enter the name of first subject");
+// let subject_two = prompt("Enter the name of second subject");
+// let subject_three = prompt("Enter the name of third subject");
+// let total_Marks = 100;
+
+
