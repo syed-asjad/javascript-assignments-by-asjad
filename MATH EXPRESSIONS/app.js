935372584e560ca503634bf1438119e71734db93
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-19 06:16:16 +0500

    Add files via upload

---
+++ MATH EXPRESSIONS/app.js
@@ -0,0 +1,31 @@
+// MATH EXPRESSIONS
+
+// let a = +prompt("Enter a number")
+// console.log("The value of a is :", a );
+// console.log("The value of ++a is :", ++a );
+// console.log("Now the value of a is : ", a);
+// console.log("==========================================")
+// console.log("The value of a++ is : ", a++ );
+// console.log("Now the value of a is :", a);
+// console.log("==========================================")
+// console.log("The value of --a is :", --a);
+// console.log("Now the value of a is :", a);
+// console.log("==========================================")
+// console.log("The value of a-- is :", a--);
+// console.log("Now the value of a is :", a);
+// console.log("==========================================")
+// let a = 2;
+// let b = 1;
+// console.log("The given values are: ");
+// console.log("The value of a is : ", a);
+// console.log("The value of b is : ", b);
+// let result = (--a) - (--b) + (++b) + b-- ;
+// console.log("First the result of --a is :", --a);
+// console.log("==========================================")
+// console.log("Second the result of --a - --b is : ", (--a) - (--b));
+// console.log("==========================================")
+// console.log("First the result of --a - --b + ++b is: ", (--a) - (--b) + (++b));
+// console.log("==========================================")
+// console.log("Third the Result of --a - --b + ++b + b--; is :", (--a) - (--b) + (++b) + (b--));
+// console.log("==========================================")
+// console.log("Now the final Result is :", result);
