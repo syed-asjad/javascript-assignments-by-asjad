366907149551fe239274e7f0f5a8dae0ed16890e
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-21 17:18:43 +0500

    app.js

--- USER INPUT & CONDITIONAL/app.js
+++ USER INPUT & CONDITIONAL/app.js
@@ -1,16 +1,21 @@
 // USER INPUT & CONDITIONAL STATEMENT
 
-let city_name = prompt("Enter a City Name").toLowerCase
-if(city_name == karachi){
 
-    console.log("Welcome to city of lights");
+let city_Name = prompt("Enter a City Name").toLowerCase();
 
-    elseif(city_name == Lahore).toLowerCase
+if(city_Name === "karachi"){
+
+    console.log("Welcome to the city of lights");
+    document.write("Welcome to the city of lights");
+}
+
+    else if(city_Name === "lahore"){
 
     console.log("Welcome to city of Mughals");
+    document.write("Welcome to the city of Mughals");
 
-}
-else{
+} else{
     
     console.log("You have Entered a wrong city");
+    document.write("You have Entered a wrong city");
 }
