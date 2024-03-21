01aea438d4f735fa4d4189d2fcbbf038b23a1a73
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-21 17:26:02 +0500

    app.js

--- USER INPUT & CONDITIONAL/app.js
+++ USER INPUT & CONDITIONAL/app.js
@@ -1,21 +1,42 @@
-// USER INPUT & CONDITIONAL STATEMENT
+// // USER INPUT & CONDITIONAL STATEMENT
 
 
-let city_Name = prompt("Enter a City Name").toLowerCase();
+// let city_Name = prompt("Enter a City Name:").toLowerCase();
 
-if(city_Name === "karachi"){
+// if(city_Name === "karachi"){
 
-    console.log("Welcome to the city of lights");
-    document.write("Welcome to the city of lights");
-}
+//     console.log("Welcome to the city of lights");
+//     document.write("Welcome to the city of lights");
+// }
 
-    else if(city_Name === "lahore"){
+//     else if(city_Name === "lahore"){
 
-    console.log("Welcome to city of Mughals");
-    document.write("Welcome to the city of Mughals");
+//     console.log("Welcome to city of Mughals");
+//     document.write("Welcome to the city of Mughals");
 
-} else{
+// } else{
     
-    console.log("You have Entered a wrong city");
-    document.write("You have Entered a wrong city");
-}
+//     console.log("You have Entered a wrong city");
+//     document.write("You have Entered a wrong city");
+// }
+
+// let gender = prompt("Enter a Gender I will print a welcome message for you:").toLowerCase();
+
+// if(gender === "male"){
+
+//     console.log("Good Morning Sir.");
+//     document.write("Good Morning Sir.");
+// }
+
+// else if(gender === "female") {
+
+//     console.log("Good Morning Mam.");
+//     document.write("Good Morning Mam.");
+// }
+
+// else{
+
+//     console.log("You have entered a wrong gender.");
+//     document.write("You have entered a wrong gender.");
+// }
+
