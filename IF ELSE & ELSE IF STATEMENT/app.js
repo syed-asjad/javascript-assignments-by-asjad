62e8d804f9ddb7ea7d0ea0e96cb794f2aadb48b3
Syed Asjad <57310761+syed-asjad@users.noreply.github.com>
2024-03-30 16:44:18 +0500

    Add files via upload

---
+++ IF ELSE & ELSE IF STATEMENT/app.js
@@ -0,0 +1,56 @@
+// // USER INPUT & CONDITIONAL STATEMENT
+
+// let city_name = prompt("Enter a City Name").toLowerCase
+// if(city_name == karachi){
+
+//     console.log("Welcome to city of lights");
+
+//     elseif(city_name == Lahore).toLowerCase
+
+//     console.log("Welcome to city of Mughals");
+
+// }
+// else{
+
+//     console.log("You have Entered a wrong city");
+// }
+
+// let user_Input = prompt(
+//   "Enter a number or any alphabet i will tell you the given input is a number or alphabet lower case or upper case"
+// );
+
+// if (!isNaN(user_Input)) {
+//   console.log(`The given input is a number and it's: ${user_Input}`);
+//   document.write(`The given input is a number and it's: ${user_Input}`);
+// } else if (user_Input >= 'A' && user_Input <= 'Z') {
+//   console.log(`The given input is upper case letter and it's: ${user_Input}`);
+//   document.write(
+//     `The given input is upper case letter and it's: ${user_Input}`
+//   );
+// } else if (user_Input >= 'a' && user_Input <= 'z') {
+//   console.log(`The given input is lower case letter and it's: ${user_Input}`);
+//   document.write(
+//     `The given input is lower case letter and it's: ${user_Input}`
+//   );
+// } else {
+//   console.log("The given input is invalid");
+//   document.write("The given input is invalid");
+// }
+
+// let first_Integer = parseInt(prompt("Enter First Number"));
+
+// let second_Integer = parseInt(prompt("Enter Second Number"));
+
+// if (first_Integer === second_Integer) {
+//   console.log("Both Numbers Are Equal");
+//   document.write("Both Numbers Are Equal");
+// } else if (first_Integer > second_Integer) {
+//   console.log("First Number is Greater");
+//   document.write("First Number is Greater");
+// } else if (first_Integer < second_Integer) {
+//   console.log("Second Number is Greater");
+//   document.write("Second Number is Greater");
+// } else {
+//   console.log("Invalid input");
+//   document.write("Invalid input");
+// }
